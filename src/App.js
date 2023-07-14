77b87d37fdc3aec9b2ce584f8681f70ce2e4f005
Ram <ram.om3152@gmail.com>
2023-07-15 00:36:44 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { NavBar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
